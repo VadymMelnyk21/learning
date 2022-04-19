28c88ed0560ae2952c28a436402b7182d6f14a8b
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-04-19 23:12:19 +0300

    отпрител

--- js/Мастерская_Tabs.js
+++ js/Мастерская_Tabs.js
@@ -1,40 +1,40 @@
 const refs = {
-    controls: document.querySelector('#tabs-1 [data-controls]'),
-    panes: document.querySelector('#tabs-1 [data-panes]'),
+  controls: document.querySelector('#tabs-1 [data-controls]'),
+  panes: document.querySelector('#tabs-1 [data-panes]'),
 };
 
-refs.controls.addEventListener('click', onControlsClick)
+refs.controls.addEventListener('click', onControlsClick);
 
 function onControlsClick(event) {
-event.preventDefault();
+  event.preventDefault();
 
-    if (event.target.nodeName !== 'A') {
-        console.log('Клик не по ссылке');
-        return;
-    }
+  if (event.target.nodeName !== 'A') {
+    console.log('Клик не по ссылке');
+    return;
+  }
 
-    const currentControl = refs.controls.querySelector('.controls__item--active')
-    
-    if (currentControl) {
-        currentControl.classList.remove('controls__item--active')
+  const currentControl = refs.controls.querySelector('.controls__item--active');
 
-        const paneId = getPaneId(currentControl)
-        const pane = getPaneById(paneId);
-        pane.classList.remove('pane--active');
-    }
+  if (currentControl) {
+    currentControl.classList.remove('controls__item--active');
 
-    const controlItem = event.target;
-    controlItem.classList.add('controls__item--active')
-
-    const paneId = getPaneId(controlItem)
+    const paneId = getPaneId(currentControl);
     const pane = getPaneById(paneId);
-    pane.classList.add('pane--active')
+    pane.classList.remove('pane--active');
+  }
+
+  const controlItem = event.target;
+  controlItem.classList.add('controls__item--active');
+
+  const paneId = getPaneId(controlItem);
+  const pane = getPaneById(paneId);
+  pane.classList.add('pane--active');
 }
 
 function getPaneId(control) {
-    return control.getAttribute('href').slice(1);
+  return control.getAttribute('href').slice(1);
 }
 
 function getPaneById(id) {
-    return refs.panes.querySelector(`#${id}`);
+  return refs.panes.querySelector(`#${id}`);
 }
