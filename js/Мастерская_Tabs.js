102e38eaed564f9724a12f7b90f0246a60425b42
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-04-19 23:09:15 +0300

    рефакторинг кода

--- js/Мастерская_Tabs.js
+++ js/Мастерская_Tabs.js
@@ -3,8 +3,10 @@ const refs = {
     panes: document.querySelector('#tabs-1 [data-panes]'),
 };
 
-refs.controls.addEventListener('click', event => {
-    event.preventDefault();
+refs.controls.addEventListener('click', onControlsClick)
+
+function onControlsClick(event) {
+event.preventDefault();
 
     if (event.target.nodeName !== 'A') {
         console.log('Клик не по ссылке');
@@ -16,17 +18,23 @@ refs.controls.addEventListener('click', event => {
     if (currentControl) {
         currentControl.classList.remove('controls__item--active')
 
-        const paneId = currentControl.getAttribute('href').slice(1);
-        const pane = refs.panes.querySelector(`#${paneId}`);
+        const paneId = getPaneId(currentControl)
+        const pane = getPaneById(paneId);
         pane.classList.remove('pane--active');
     }
 
     const controlItem = event.target;
     controlItem.classList.add('controls__item--active')
 
-    const paneId = controlItem.getAttribute('href').slice(1);
-
-    const pane = refs.panes.querySelector(`#${paneId}`);
+    const paneId = getPaneId(controlItem)
+    const pane = getPaneById(paneId);
     pane.classList.add('pane--active')
+}
 
-})
+function getPaneId(control) {
+    return control.getAttribute('href').slice(1);
+}
+
+function getPaneById(id) {
+    return refs.panes.querySelector(`#${id}`);
+}
