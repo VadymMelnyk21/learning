d79790393be2a7941766d23e02d286e64c65c271
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-04-20 23:50:16 +0300

    функционал ванилы

--- js/Мастерская_Tabs_Плагин.js
+++ js/Мастерская_Tabs_Плагин.js
@@ -1,18 +1,73 @@
 class Tabs {
-    constructor({ rootSelector }) {
+    constructor({
+        rootSelector,
+        activeControlClass = 'active',
+        activePaneClass = 'active',
+
+    }) {
         this._refs = this._getRefs(rootSelector);
+        this._activeControlClass = activeControlClass;
+        this._activePaneClass = activePaneClass;
+
+        this._bindEvents();
 
     }
+
     _getRefs(root) {
         const refs = {};
 
         refs.controls = document.querySelector(`${root} [data-controls]`);
-        refs.panes = document.querySelector(`${toot} data-panes`);
+        refs.panes = document.querySelector(`${root} [data-panes]`);
 
         return refs;
     }
+
+    _bindEvents() {
+        this._refs.controls.addEventListener(
+            'click',
+            this._onControlsClick.bind(this));
+    }
+
+    _onControlsClick(event) {
+        event.preventDefault();
+
+        if (event.target.nodeName !== 'A') {
+            console.log('Клик не по ссылке');
+            return;
+        }
+
+        const currentControl = this._refs.controls.querySelector(
+            `.${this._activeControlClass}`);
+
+
+        if (currentControl) {
+            currentControl.classList.remove(this._activeControlClass);
+
+            const paneId = this._getPaneId(currentControl);
+            const pane = this._getPaneById(paneId);
+            pane.classList.remove(this._activePaneClass);
+        }
+
+        const controlItem = event.target;
+        controlItem.classList.add(this._activeControlClass);
+
+        const paneId = this._getPaneId(controlItem);
+        const pane = this._getPaneById(paneId);
+        pane.classList.add(this._activePaneClass);
+    }
+
+    _getPaneId(control) {
+        return control.getAttribute('href').slice(1);
+    }
+
+    _getPaneById(id) {
+        return this._refs.panes.querySelector(`#${id}`);
+    }
+
 }
 
 const tabs1 = new Tabs({
     rootSelector: '#tabs-1',
-})
+    activeControlClass: 'controls__item--active',
+    activePaneClass: 'pane--active',
+});
