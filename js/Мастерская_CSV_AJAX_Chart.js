cc681c1ddae71e20768ec00b81ae9a46161028a6
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-04-23 18:24:48 +0300

    CSV AJAX Chart

--- js/Мастерская_CSV_AJAX_Chart.js
+++ js/Мастерская_CSV_AJAX_Chart.js
@@ -7,6 +7,7 @@ fetchData()
     .then(({ years, temps, north, south }) => drawChart(years, temps, north, south))
 
 function fetchData() {
+    // return fetch('../DataBase/ZonAnn.Ts+dSST.csv') для использования локально на компе
     return fetch('https://vadymmelnyk21.github.io/learning/DataBase/ZonAnn.Ts+dSST.csv')
         .then(res => res.text())
 }
