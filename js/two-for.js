e52abff8d95452ee0012b35b4777b3f7fb259611
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-04-26 22:48:45 +0300

    цикл фор

--- js/two-for.js
+++ js/two-for.js
@@ -1,1 +1,39 @@
 'use strict'
+
+const products = [{
+    id: 'sku1',
+    qty: 1,
+}, {
+    id: 'sku2',
+    qty: 2,
+}, {
+    id: 'sku3',
+    qty: 3,
+}, {
+    id: 'sku1',
+    qty: 6,
+}, {
+    id: 'sku1',
+    qty: 8,
+}, {
+    id: 'sku2',
+    qty: 19,
+}, {
+    id: 'sku4',
+    qty: 1,
+}];
+
+for (let i = 0; i < products.length; i += 1) {
+    console.log(products[i]);
+    for (let j = i + 1; j < products.length; j += 1) {
+        console.log(products[i]);
+        console.log(products[j]);
+
+        if (products[i].id === products[j].id) {
+            products[i].qty += products[j].qty
+            products.splice(j, 1)
+            j -= 1
+        }
+    }
+}
+console.log(products);
