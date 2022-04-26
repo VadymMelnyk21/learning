6ba6f2d30f58e2a67e3c0c71f9e2951bdda5d18e
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-04-26 23:15:26 +0300

    for 2 обєкт

--- js/two-for.js
+++ js/two-for.js
@@ -1,5 +1,8 @@
 'use strict'
 
+// Знайти об’єкти з однаковими id
+// Параметри qty додти а однакові id видалити
+
 const products = [{
     id: 'sku1',
     qty: 1,
@@ -37,3 +40,32 @@ for (let i = 0; i < products.length; i += 1) {
     }
 }
 console.log(products);
+
+///////////////////////////////////////////////////////////////
+
+// Мутувати обєкт так щоб в ньому були тільки унікальні імена
+// Потім перебрати його і вивести імена по черзі в консолі
+// Використовувати цикл for для перебору
+
+const names = {
+    user: 'Artem',
+    user1: 'Denis',
+    user2: 'Kate',
+    user3: 'Dima',
+    user4: 'Denis',
+    user5: 'Denis',
+    user6: 'Kate',
+    user7: 'Kristina'
+}
+
+const updateNames = Object.keys(names);
+
+for (let i = 0; i < updateNames.length; i += 1) {
+
+    for (let j = i + 1; j < updateNames.length; j += 1) {
+        if (names[updateNames[i]] === names[updateNames[j]]) {
+            delete names[updateNames[j]]
+        }
+    }
+}
+console.log(names);
